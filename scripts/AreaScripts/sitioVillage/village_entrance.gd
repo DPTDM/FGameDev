11ae66aad89a49372e7e73577636865d78a25822
DPTDM <22105026@usc.edu.ph>
2026-05-02 19:39:04 +0800

    more areas
    No Hitboxes just yet

--- scripts/AreaScripts/sitioVillage/village_entrance.gd
+++ scripts/AreaScripts/sitioVillage/village_entrance.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var next_area_path: String = "res://scenes/GameLevels/Areas/train_station.tscn"
-@export var entry_group: String = "station_entrance"
+@export var entry_group: String = "train_entrance"
 
 func _on_body_entered(body):
 	if body.name == "Player":
