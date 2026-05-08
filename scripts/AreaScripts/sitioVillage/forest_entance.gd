086c42bf5877f7492711a986210c2a75eaa5734f
DPTDM <22105026@usc.edu.ph>
2026-05-08 11:00:33 +0800

    new Area

---
+++ scripts/AreaScripts/sitioVillage/forest_entance.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@export var next_area_path: String = "res://scenes/GameLevels/Areas/forest_to_temple.tscn"
+@export var entry_group: String = "forest_entrance"
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		var gameplay = get_tree().current_scene
+		gameplay.load_area(next_area_path, entry_group)
