34a1615045823dafbed6db932ec6735b0ffba0ee
Lloyd <antonmontero005@gmail.com>
2026-05-09 11:52:44 +0800

    Added chase scenes and update scene transition

---
+++ scripts/AreaScripts/Chase/chase.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@export var next_area_path: String = "res://scenes/GameLevels/Areas/boss_fight_scene.tscn"
+@export var entry_group: String = "p_spawn"
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		var gameplay = get_tree().current_scene
+		gameplay.load_area(next_area_path, entry_group)
